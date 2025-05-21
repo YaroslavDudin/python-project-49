5e2536c172c9e55e623c165f004120dba909722e
YaroslavDudin <yaroslavedudin@yandex.ru>
2025-05-21 06:04:18 +0300

    rem old comment

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -4,7 +4,6 @@ rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number):
-    """Checks if a number is prime."""
     if number < 2:
         return False
     for i in range(2, int(number**0.5) + 1):
