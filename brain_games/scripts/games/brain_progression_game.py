03112a0d0aa64f05ff27c6c5d471fb18dc21eea1
YaroslavDudin <yaroslavedudin@yandex.ru>
2025-05-21 05:22:17 +0300

    fix progression

--- brain_games/scripts/games/brain_progression_game.py
+++ brain_games/scripts/games/brain_progression_game.py
@@ -42,7 +42,7 @@ def main():
             correct_answers_count += 1
         else:
             print(f"'{answer}' is wrong answer ;(. "
-            f"Correct answer was '{correct_answer}'.")
+            f"Correct answer was '{hidden_number}'.")
     
             print(f"Let's try again, {name}!")
             return
